ccd63ef6c2ab54be1e4f47fc5cbf440295cd6b8c
Дмитрий Карпов <>
2019-03-02 23:02:50 +0300

    Создан класс MemoCard. Изменили кортеж на список экземпляров класса MemoCards

---
+++ MemoCardModule.swift
@@ -0,0 +1,23 @@
+//
+//  MemoCardModule.swift
+//  MemoCards
+//
+//  Created by Дмитрий Карпов on 02/03/2019.
+//  Copyright © 2019 MacKarpov. All rights reserved.
+//
+
+import Foundation
+
+class MemoCard {
+    let keyNumber: String
+    let keyWord: String
+    let keyLetters: String
+    let fileName: String
+
+    init(keyNumber: String, keyWord: String, keyLetters: String, fileName: String) {
+        self.keyNumber = keyNumber
+        self.keyWord = keyWord
+        self.keyLetters = keyLetters
+        self.fileName = fileName
+    }
+}
